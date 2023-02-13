260291087976bd9b2067e34e4384681dd6a4d562
Mayur <mayurjreddy@gmail.com>
2023-02-12 23:17:50 -0500

    first commit

---
+++ Source/ParkourFPSCPP/ParkourFPSCPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ParkourFPSCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOURFPSCPP_API AParkourFPSCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
