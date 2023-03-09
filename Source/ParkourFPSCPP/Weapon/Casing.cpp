39cd17e2a9927759be7e54aa8252e8b8a770e796
Mayur <mayurjreddy@gmail.com>
2023-03-09 11:56:00 -0500

    Shell Physics

---
+++ Source/ParkourFPSCPP/Weapon/Casing.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
+ACasing::ACasing()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
+	SetRootComponent(CasingMesh);
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+	ShellEjectionImpulse = 10.f;
+	Lifespan = 3.f;
+}
+
+void ACasing::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* Other, FVector NormalImpulse, const FHitResult& Hit)
+{
+
+	if (ShellSound) {
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+
+	GetWorldTimerManager().SetTimer(TimerHandle, [this]() {
+		Destroy();
+		}, Lifespan, false);
+}
+
+
